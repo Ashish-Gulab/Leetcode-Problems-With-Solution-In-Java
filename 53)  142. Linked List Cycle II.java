1d3e973a3a5d1bcc0a0cedfbc4bd8737c09b2227
Ashish Gulab <ashishgulab.gulab@gmail.com>
2023-02-27 18:53:03 +0530

    Create 53)  142. Linked List Cycle II.java

---
+++ 53)  142. Linked List Cycle II.java
@@ -0,0 +1,90 @@
+/******************************************************************  Problem Statement   *******************************************************************************/
+
+
+Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+
+Do not modify the linked list.
+
+Example 1:
+
+Input: head = [3,2,0,-4], pos = 1
+Output: tail connects to node index 1
+Explanation: There is a cycle in the linked list, where tail connects to the second node.
+
+Example 2:
+
+Input: head = [1,2], pos = 0
+Output: tail connects to node index 0
+Explanation: There is a cycle in the linked list, where tail connects to the first node.
+
+Example 3:
+
+Input: head = [1], pos = -1
+Output: no cycle
+Explanation: There is no cycle in the linked list.
+ 
+Constraints:
+
+The number of the nodes in the list is in the range [0, 104].
+-105 <= Node.val <= 105
+pos is -1 or a valid index in the linked-list.
+
+
+/*********************************************************************  Solution   **********************************************************************************/
+
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+
+    public ListNode intersection(ListNode head)
+    {
+        ListNode turtle=head;
+        ListNode hare=head;
+        while(hare.next!=null && hare.next.next!=null)
+        {
+            turtle=turtle.next;
+            hare=hare.next.next;
+
+            if(turtle==hare)
+            {
+                return turtle;
+            }
+        }
+        return null;
+    }
+    
+    public ListNode detectCycle(ListNode head) {
+
+        if(head==null || head.next==null)
+        {
+            return null;
+        }
+
+        ListNode intersect=intersection(head);
+        ListNode start=head;
+
+        if(intersect==null)
+        {
+            return null;
+        }
+
+        while(start!=intersect)
+        {
+            start=start.next;
+            intersect=intersect.next;
+        }
+        return start;
+    }
+}
